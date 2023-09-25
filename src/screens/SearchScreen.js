297ba795a8cb04982f788bd9f0a7e4910ef2695e
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-09-25 14:48:48 +0530

    Assembled a Navigator for Our App

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>This is the Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
